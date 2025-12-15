c3403555ba0f8a286de53e08218b92a3a2531c27
Noah González Rodríguez <noah.gonzalezro@elorrieta-errekamari.com>
2025-12-15 19:57:25 +0100

    junit da error

---
+++ src/ejercicio/CalculadoraTest.java
@@ -0,0 +1,29 @@
+package ejercicio;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+class CalculadoraTest {
+
+	@Test
+	void testCalculadora() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testResta() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testResta2() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testDivide2() {
+		fail("Not yet implemented");
+	}
+
+}
